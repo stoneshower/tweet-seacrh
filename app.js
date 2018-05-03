b8cc156c0428ac3bc9065a9ccd5ac821c0c03343
Shinya Takahashi <shinyatkhs@gmail.com>
2018-05-03 15:49:03 +0900

    updated port on router

--- app.js
+++ app.js
@@ -176,6 +176,5 @@ app.get('/tweet_user', (req, res, next) => {
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
 
-httpServer.listen(process.env.PORT || 3000)
-httpsServer.listen(process.env.PORT || 4435)
-console.log(`listen on port 4435`)
+// httpServer.listen(process.env.PORT || 3000)
+httpsServer.listen(process.env.PORT)
